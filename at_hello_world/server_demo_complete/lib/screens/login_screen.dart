9ab557628b118760749c05f8b311f7ddb191de8d
Sai Rama <sairama-vanga>
2021-01-12 12:31:00 +0530

    authentication fix in at_hello-world

--- at_hello_world/server_demo_complete/lib/screens/login_screen.dart
+++ at_hello_world/server_demo_complete/lib/screens/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:newserverdemo/screens/home_screen.dart';
 import 'package:newserverdemo/services/server_demo_service.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:at_demo_data/at_demo_data.dart' as at_demo_data;
 
 String atSign;
 
@@ -95,12 +96,13 @@ class _LoginScreenState extends State<LoginScreen> {
     setState(() {
       showSpinner = true;
     });
+    String cramSecret = at_demo_data.cramKeyMap[atSign];
     if (atSign != null) {
-      _serverDemoService.onboard(atsign: atSign).then((value) {
+      _serverDemoService.onboard().then((value) {
         Navigator.pushReplacementNamed(context,
             HomeScreen.id);
       }).catchError((error) async {
-        await _serverDemoService.authenticate(atSign);
+        await _serverDemoService.authenticate(atSign,cramSecret:cramSecret );
         Navigator.pushReplacementNamed(context,
             HomeScreen.id);
       });
