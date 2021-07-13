a06075c8d359caf12a929890f523ecfc6ba1f16b
Intiser Ahmed <57252940+Intiserahmed@users.noreply.github.com>
2021-07-13 19:45:44 +0600

    Delete at_hello_world directory

--- at_hello_world/server_demo_incomplete/lib/utils/at_conf.dart
+++
@@ -1,7 +0,0 @@
-class AtConfig {
-  static const String root = 'root.atsign.org';
-  static const int rootPort = 64;
-  static const String namespace = 'server_demo';
-  static const bool debug = true;
-  static const bool verbose = true;
-}
