5fb8d2e8c287ff2b011531ed3f00d16defc65f4a
gkc <gary.casey@gmail.com>
2022-02-24 16:17:42 +0000

    example doesn't exit

--- dart/max30101/example/max30101_example.dart
+++ dart/max30101/example/max30101_example.dart
@@ -1,19 +1,10 @@
-import 'dart:io';
-
 import 'package:max30101/max30101.dart';
 import 'package:max30101/src/real_i2c_wrapper.dart';
 
 late MAX30101 pulseOxymeter;
 
-int maxCallbacks = 100;
-int numCallbacks = 0;
-
 void onBeat(bool beatDetected, double bpm, double sao2) {
   print ("onBeat called with beatDetected:$beatDetected bpm:$bpm sao2:$sao2");
-  if (++numCallbacks >= maxCallbacks) {
-    print ("Exiting");
-    exit(0);
-  }
 }
 
 void main() {
