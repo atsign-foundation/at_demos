3d60ac3048981cdcb44962f3295ee83541f29aad
gkc <gary.casey@gmail.com>
2022-02-28 19:26:02 +0100

    iot_sensor_sender was surfacing weird interactions between reading from the sensor and communicating via atClient. So renamed to iot_sensor_publisher.dart and its sole job is now to make sensor readings and publish onto mqtt

---
+++ dart/iot_sender/bin/iot_sensor_publisher.dart
@@ -0,0 +1,50 @@
+import 'package:max30101/max30101.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'dart:io';
+
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+void main(List<String> arguments) async {
+  /// This sender integrates with the sensor and publishes on 'mwc_beat_hr_o2' topic
+  /// Which in turn is subscribed to by (a) the python TFT driver (b) the iot_mqtt_listener
+
+  /// The MAX30101 sensor driver
+  printWithTimestamp("Starting Max30101 sampler");
+
+  final publishingClient = MqttServerClient('localhost', '');
+  try {
+    await publishingClient.connect();
+  } on NoConnectionException catch (e) {
+    // Raised by the client when connection fails.
+    printWithTimestamp('client exception - $e');
+    publishingClient.disconnect();
+  } on SocketException catch (e) {
+    // Raised by the socket layer
+    printWithTimestamp('socket exception - $e');
+    publishingClient.disconnect();
+  }
+
+  /// Check we are connected
+  if (publishingClient.connectionStatus!.state == MqttConnectionState.connected) {
+    printWithTimestamp('Mosquitto publishing client connected');
+  } else {
+    /// Use status here rather than state if you also want the broker return code.
+    printWithTimestamp('ERROR Mosquitto publishing client connection failed - disconnecting, status is ${publishingClient.connectionStatus}');
+    publishingClient.disconnect();
+    exit(-1);
+  }
+
+  const String publishTopic = 'mqtt/mwc_beat_hr_o2';
+
+  void onBeat(bool beatDetected, double bpm, double sao2) {
+    printWithTimestamp("onBeat called with beatDetected:$beatDetected bpm:$bpm sao2:$sao2 : publishing to $publishTopic");
+    String message = '${beatDetected.toString().toLowerCase()},${bpm.toStringAsFixed(1)},${sao2.toStringAsFixed(1)}';
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+    publishingClient.publishMessage(publishTopic, MqttQos.atLeastOnce, builder.payload!);
+    printWithTimestamp("onBeat published $message to $publishTopic");
+  }
+
+  MAX30101 max30101 = MAX30101(RealI2CWrapper(1), false, debug:false);
+  max30101.runSampler(onBeat);
+}
