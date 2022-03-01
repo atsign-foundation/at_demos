09d0f9fee72af073dab12ec6e5d1c2e822148f68
gkc <gary.casey@gmail.com>
2022-03-01 08:40:58 +0100

    Add 'mode' command-line argument {'hr','o2','hro2'} to iot_sensor_publisher

--- dart/iot_sender/bin/iot_sensor_publisher.dart
+++ dart/iot_sender/bin/iot_sensor_publisher.dart
@@ -5,6 +5,35 @@ import 'dart:io';
 import 'package:mqtt_client/mqtt_server_client.dart';
 
 void main(List<String> arguments) async {
+  void printUsage() {
+    print('Usage: iot_sensor_publisher <mode>');
+    print("    where <mode> is one of {hr|o2|hro2}");
+    exit(0);
+  }
+
+  if (arguments.isEmpty || arguments.length > 1) {
+    printUsage();
+  }
+
+  bool sendHR = false;
+  bool sendO2 = false;
+
+  String mode = arguments[0].toLowerCase();
+  switch(mode) {
+    case 'hr':
+      sendHR = true;
+      break;
+    case 'o2':
+      sendO2 = true;
+      break;
+    case 'hro2':
+      sendHR = true;
+      sendO2 = true;
+      break;
+    default:
+      printUsage();
+  }
+
   /// This sender integrates with the sensor and publishes on 'mwc_beat_hr_o2' topic
   /// Which in turn is subscribed to by (a) the python TFT driver (b) the iot_mqtt_listener
 
@@ -37,6 +66,13 @@ void main(List<String> arguments) async {
   const String publishTopic = 'mqtt/mwc_beat_hr_o2';
 
   void onBeat(bool beatDetected, double bpm, double sao2) {
+    if (! sendHR) {
+      bpm = 0.0;
+    }
+    if (! sendO2) {
+      sao2 = 0.0;
+    }
+
     printWithTimestamp("onBeat called with beatDetected:$beatDetected bpm:$bpm sao2:$sao2 : publishing to $publishTopic");
     String message = '${beatDetected.toString().toLowerCase()},${bpm.toStringAsFixed(1)},${sao2.toStringAsFixed(1)}';
     final builder = MqttClientPayloadBuilder();
