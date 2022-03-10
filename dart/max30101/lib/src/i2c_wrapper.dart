7bca66fa848c79181a0af4e0440e6370089e484f
gkc <gary.casey@gmail.com>
2022-03-10 13:02:28 +0000

    Split out max30101 package into its own repo
    * Moved dart/max30101 from this repo to its own repo at https://github.com/atsign-foundation/max30101 (and published to pub.dev from there)
    * Modified pubpsec for dart/iot_sender to pick up the max30101 package from pub.dev

--- dart/max30101/lib/src/i2c_wrapper.dart
+++
@@ -1,5 +0,0 @@
-abstract class I2CWrapper {
-  int readByteReg(int address, int register);
-  void writeByteReg(int address, int register, int byteValue);
-  List<int> readBytesReg(int address, int register, int len);
-}
