44b2faa87f3e957394cb61384a63f3e2901034ed
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-29 16:53:12 -0400

    chore: `<ip>` placeholder

--- sshnp_docker_demo/sshrvd/.startup.sh
+++ sshnp_docker_demo/sshrvd/.startup.sh
@@ -4,6 +4,6 @@ ssh-keygen -o -a 100 -t ed25519 -f /atsign/.ssh/id_ed25519 -C "john@example.com"
 /usr/sbin/sshd -D -o "ListenAddress 127.0.0.1" -o "PasswordAuthentication no"  &
 while true
 do
-sudo -u atsign /atsign/sshnp/sshrvd -a @sshrvd -i 172.17.0.2 -v -s
+sudo -u atsign /atsign/sshnp/sshrvd -a @sshrvd -i <ip> -v -s
 sleep 3
 done
