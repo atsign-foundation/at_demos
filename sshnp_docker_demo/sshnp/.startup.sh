04153d2d2437e387fd581512d52a5ce455a901be
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-05-29 14:45:19 -0400

    feat: shell script place holders

--- sshnp_docker_demo/sshnp/.startup.sh
+++ sshnp_docker_demo/sshnp/.startup.sh
@@ -2,4 +2,4 @@
 ssh-keygen -A
 ssh-keygen -o -a 100 -t ed25519 -f /atsign/.ssh/id_ed25519 -C "john@example.com"
 /usr/sbin/sshd -D -o "ListenAddress 127.0.0.1" -o "PasswordAuthentication no"  &
-sudo -u atsign /atsign/sshnp/sshnp -f @soccer99 -t @66dear32 -h @48leo -d docker -s id_ed25519.pub -v
+sudo -u atsign /atsign/sshnp/sshnp -f @sshnp -t @sshnpd -h @sshrvd -d deviceName -s id_ed25519.pub -v
--- sshnp_docker_demo/sshnpd/.startup.sh
+++ sshnp_docker_demo/sshnpd/.startup.sh
@@ -3,6 +3,6 @@ ssh-keygen -A
 /usr/sbin/sshd -D -o "ListenAddress 127.0.0.1" -o "PasswordAuthentication no"  &
 while true
 do
-sudo -u atsign /atsign/sshnp/sshnpd -a @66dear32 -m @soccer99 -d docker -s -u -v
+sudo -u atsign /atsign/sshnp/sshnpd -a @sshnpd -m @sshnp -d deviceName -s -u -v
 sleep 3
 done
