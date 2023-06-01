924f13bd7cbaff89bfc76219730d0dd4156d9ccb
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-06-01 19:16:24 -0400

    chore: use <> for token substitution

--- sshnp_docker_demo/sshnpd/.startup.sh
+++ sshnp_docker_demo/sshnpd/.startup.sh
@@ -3,6 +3,6 @@ ssh-keygen -A
 /usr/sbin/sshd -D -o "ListenAddress 127.0.0.1" -o "PasswordAuthentication no"  &
 while true
 do
-sudo -u atsign /atsign/sshnp/sshnpd -a @sshnpd -m @sshnp -d deviceName -s -u -v
+sudo -u atsign /atsign/sshnp/sshnpd -a <@sshnpd> -m <@sshnp> -d deviceName -s -u -v
 sleep 3
 done
